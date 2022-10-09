1e6e4f7b1811ecdc990e8b9acb25af7ae3c3aebf
007-jcs <106826942+007-jcs@users.noreply.github.com>
2022-10-09 17:52:32 -0400

    hero section

---
+++ src/pages/about.js
@@ -0,0 +1,18 @@
+import React from 'react';
+
+import "../components/index.css"
+
+
+const about = () => {
+  return (
+    <header className='hero'>
+      <div className='heroContainer'>
+        <div className='heroInfo'>
+        <h1>The about page</h1>
+                </div>
+      </div>
+    </header>
+  );
+}
+
+export default about;
