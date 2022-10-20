18a6b48678d7ddcad82863eeb4f263e52ff8befa
Moonraker79 <106351067+Moonraker79@users.noreply.github.com>
2022-10-20 15:11:15 -0400

    new site

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,20 +1,20 @@
-import React from 'react';
-import { Link } from 'gatsby';
+import React from 'react'
 import "../components/index.css"
 import { navigate } from "gatsby"
-
+import "../components/index.css"
+import { Link } from 'gatsby';
 const index = () => {
   return (
     <header className='hero'>
-      <div className='heroContainer'>
-        <div className='heroInfo'>
-        <h1>Jeff C. Salter</h1>
-        <p>Cybersecurity and Information Design</p>
-        <button className='heroButton' onClick={()=>{navigate("/about")}}>How We Can Help</button>
-        </div>
-      </div>
-    </header>
-  );
+    <div className='heroContainer'>
+       <div className='heroInfo'>
+       <h1>Jeff C. Salter</h1>
+       <p>Cybersecurity and Information Design</p>
+       <button className='heroButton' onClick={()=>{navigate("/about")}}>How We Can Help</button>
+       </div>
+     </div>
+   </header> 
+  ) 
 }
 
-export default index;
+export default index
