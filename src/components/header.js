4749e563df7f2c509828aa9bbf13304d36c4ff38
Moonraker79 <106351067+Moonraker79@users.noreply.github.com>
2022-10-21 17:03:08 -0400

    naviagation

---
+++ src/components/header.js
@@ -0,0 +1,24 @@
+import React from 'react'
+import Topnav from "./topnav"
+import * as heroStyles from "./hero.module.css"
+import { navigate } from "gatsby"
+
+const Header = () => {
+  return (
+    <header className={heroStyles.hero} >
+       <div>
+       <Topnav />
+       </div>
+       <div className={heroStyles.heroContainer}>
+        <div className={heroStyles.heroInfo}>
+            <h1>Jeff C. Salter</h1>
+            <p>Cybersecurity and Information Design</p>
+            <button className='heroButton' onClick={()=>{navigate("/about")}}>How We Can Help</button>
+        </div>
+       </div>
+     
+    </header>
+  )
+}
+
+export default Header
--- src/pages/index.js
+++ src/pages/index.js
@@ -1,20 +1,13 @@
 import React from 'react'
-import "../components/index.css"
-import { navigate } from "gatsby"
-import "../components/index.css"
-import { Link } from 'gatsby';
+import Layout from "../components/layout"
+ 
+
 const index = () => {
   return (
-    <header className='hero'>
-    <div className='heroContainer'>
-       <div className='heroInfo'>
-       <h1>Jeff C. Salter</h1>
-       <p>Cybersecurity and Information Design</p>
-       <button className='heroButton' onClick={()=>{navigate("/about")}}>How We Can Help</button>
-       </div>
-     </div>
-   </header> 
-  ) 
+    <Layout>
+      <h1>This is the homepage body copy section</h1>
+    </Layout>
+  )
 }
 
 export default index
