49829934c7f770211609e1a0a85d487f1a16605f
Goldfinger63 <jcsalter63@yahoo.com>
2022-11-28 14:05:41 -0500

    card update

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,6 +1,9 @@
 import React from 'react'
 import Layout from "../components/layout"
 import Card from "../components/card"
+import  "../components/index.css"
+import { Link } from "gatsby"
+
  
 
 const index = () => {
@@ -8,15 +11,22 @@ const index = () => {
     <Layout>
       <div className="Cards">
       <h2>How can we help</h2>
-      <Card 
-        title="Web Design"
-        text="Sub Title" />
+      <div className="cardGroup">
+        <div className="cardOne">
+          <Link to="/services">
+          <Card 
+            title="Web Design"
+            text="Sub Title" 
+          />
+          </Link>
+         </div>
         <Card 
         title="Custom Photography"
         text="Location or Studio" />
         <Card 
         title="Thoughts and Ideas"
         text="Let us build up your web presence " />
+        </div>
       </div>
     </Layout>
   )
