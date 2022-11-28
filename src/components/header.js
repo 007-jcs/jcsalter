8ebdec3930f21bad7ac158a5cdfa385da29150fb
Goldfinger63 <jcsalter63@yahoo.com>
2022-11-27 19:14:05 -0500

    update header

--- src/components/header.js
+++ src/components/header.js
@@ -7,7 +7,7 @@ const Header = () => {
   return (
     <header className={heroStyles.hero} >
        <div>
-       
+       <h1>This is the navigation</h1>
        </div>
        <div className={heroStyles.heroContainer}>
         <div className={heroStyles.heroInfo}>
